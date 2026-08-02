c698e235618cd1c7ca37e5b1718e6ce28da3e0d1
อัลเบิร์ส ผลบุตร <albert.phonbut@gmail.com>
2026-08-02 13:48:04 +0700

    whisper function added

--- git-for-teams/starter/team/utils.js
+++ git-for-teams/starter/team/utils.js
@@ -3,4 +3,8 @@ function capitalize(word) {
   return word[0].toUpperCase() + word.slice(1);
 }
 
-export { capitalize };
+function whisper(word) {
+  return word.toLowerCase();
+}
+
+export { capitalize, whisper };
