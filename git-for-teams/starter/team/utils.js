d5dacc9caf9e5a39846e4ba6c58f8a70d7cb7809
Poramet Nilchan <porametnilchan@gmail.com>
2026-08-02 14:47:08 +0700

    add-whisper-helper

--- git-for-teams/starter/team/utils.js
+++ git-for-teams/starter/team/utils.js
@@ -3,4 +3,8 @@ function capitalize(word) {
   return word[0].toUpperCase() + word.slice(1);
 }
 
-export { capitalize };
+function whisper(word) {
+  return word[0].toLowerCase() + word.slice(1);
+}
+
+export { capitalize, whisper };
